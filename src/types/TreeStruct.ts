5a2e54eeb3ced30827d9143a61c92f961e563a3e
Kibbweeb <ahmahsyakiebul@gmail.com>
2026-07-09 00:40:38 +0700

    adding binary search tree data struct

--- src/types/TreeStruct.ts
+++ src/types/TreeStruct.ts
@@ -1,5 +1,6 @@
 export interface DefTreeNode<T> {
     data: T;
+    id: string;
     children: DefTreeNode<T>[];
 }
 
@@ -7,8 +8,24 @@ export interface DefTree<T> {
     root: DefTreeNode<T> | null;
     size: number;
     
-    addChild(parentData: T, childData: T): boolean;
-    removeChild(data: T): boolean;
+    addChild(parentId: string, childData: T): string | null;
+    removeChild(targetId: string): boolean;
     traverseDFS(callback: (node: DefTreeNode<T>) => void): void;
     traverseBFS(callback: (node: DefTreeNode<T>) => void): void;
+}
+export interface DefBinaryTreeNode<T> {
+    data: T;
+    id: string;
+    parent: DefBinaryTreeNode<T> | null;
+    left: DefBinaryTreeNode<T> | null;
+    right: DefBinaryTreeNode<T> | null;
+}
+
+export interface DefBinaryTree<T> {
+    root: DefBinaryTreeNode<T> | null;
+    size: number;
+
+    searchNode(targetData: T): DefBinaryTreeNode<T> | null;
+    insertNode(data: T): string | null;
+    removeNode(targetId: string): boolean;
 }
